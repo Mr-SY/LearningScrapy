8e26a8e90a6bafd3e09ab3079af4491dce8be7eb
Mr-Subok <37680519+Mr-Subok@users.noreply.github.com>
2019-04-03 22:38:51 +0900

    Update emart.py

--- sproject/sproject/spiders/emart.py
+++ sproject/sproject/spiders/emart.py
@@ -1,9 +1,17 @@
 import scrapy
 
+class QuotesSpider(scrapy.Spider):
+    name = "emart"
 
-class AuthorSpider(scrapy.Spider):
-    name = 'example'
+    def start_requests(self):
+        start_url = ["http://emart.ssg.com/best/main.ssg?Egnb=best"]
 
-    start_urls=[
-        'http://www.'
-    ]
+    def parse(self, response):
+        for sel in response.xpath('//*[@id="best5"]/li'):
+            yeild {
+            'title' : sel.xpath('/div/a/text()').extract(),
+            #'link' : sel.xpath('/div/a/@href').extract(),
+            #'number' : sel.xpath('/strong/text()').extract(),
+            }
+            
+            
